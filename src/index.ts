05eed0250e91fdceea779f3001c6427ca1e74c38
unknown <izaelec@gmail.com>
2022-04-03 09:21:06 -0400

    imports modified

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-const app = express();
-const port = process.env.PORT || 5000;
 
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
-
+const port = process.env.PORT || 5000;
 
 const re = /^https:\/\/([\w-]+\.)+[\w-]+(\/[\w-]+)*\/?$/;
 
