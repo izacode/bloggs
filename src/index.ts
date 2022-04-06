58807ee04ef45566ccc5ca71cb955d336e4ccbbc
unknown <izaelec@gmail.com>
2022-04-06 14:34:16 -0400

    added lutiy govnocod

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { videosRouter } from "./routes/videos-routes";
@@ -38,26 +38,26 @@ type PostType = {
 
 type ErrorType = {
   data: {
-    additionalProp1: string;
-    additionalProp2: string;
+    id?: number;
+    name?: string;
+    youtubeURI?: string;
+    title?: string;
+    shortDescription?: string;
+    content?: string;
+    bloggerID?: string;
+    bloggerName?: string;
   };
-  errorsMessage: {
-    message: string;
-    field: string;
+  errorMessage: {
+    message?: string;
+    field?: string;
   };
   resultCode: number;
 };
 
 let error: ErrorType = {
-  data: {
-    additionalProp1: "",
-    additionalProp2: "",
-  },
-  errorsMessage: {
-    message: "",
-    field: "",
-  },
-  resultCode: 2,
+  data: {},
+  errorMessage: {},
+  resultCode: 0,
 };
 
 const bloggers: BloggerType[] = [
@@ -93,93 +93,85 @@ const posts: PostType[] = [
     id: 1,
     title: "Post-01",
     shortDescription: "First post of Blogger-01",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
     bloggerID: 1,
-    bloggerName: "Blogger-01",
   },
   {
     id: 2,
     title: "Post-01",
     shortDescription: "First post of Blogger-01",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
     bloggerID: 2,
-    bloggerName: "Blogger-02",
   },
   {
     id: 3,
     title: "Post-01",
     shortDescription: "First post of Blogger-01",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
     bloggerID: 3,
-    bloggerName: "Blogger-03",
   },
   {
     id: 4,
     title: "Post-01",
     shortDescription: "First post of Blogger-01",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
     bloggerID: 4,
-    bloggerName: "Blogger-04",
   },
   {
     id: 5,
     title: "Post-01",
     shortDescription: "First post of Blogger-01",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
     bloggerID: 5,
-    bloggerName: "Blogger-05",
   },
   {
     id: 6,
     title: "Post-02",
     shortDescription: "Second post of Blogger-01",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
     bloggerID: 1,
-    bloggerName: "Blogger-01",
   },
   {
     id: 7,
     title: "Post-02",
     shortDescription: "Second post of Blogger-02",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
     bloggerID: 2,
-    bloggerName: "Blogger-02",
   },
   {
     id: 8,
     title: "Post-02",
     shortDescription: "Second post of Blogger-03",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
     bloggerID: 3,
-    bloggerName: "Blogger-03",
   },
   {
     id: 9,
     title: "Post-02",
     shortDescription: "Second post of Blogger-04",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
     bloggerID: 4,
-    bloggerName: "Blogger-04",
   },
   {
     id: 10,
     title: "Post-02",
     shortDescription: "Second post of Blogger-05",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
     bloggerID: 5,
-    bloggerName: "Blogger-05",
   },
 ];
+
+// const checkBloggerID = (id:number, bloggers: BloggerType[], res: Response, next: NextFunction) => {
+//     const bloggerID = id;
+//     const blogger = bloggers.find((b) => b.id === bloggerID);
+//     if(!bloggerID || !blogger){
+//       return res.status(404).json({
+//         status: "fail",
+//         message: "Invalid ID  or blogger doesn't exists",
+//       });
+//     }
+//     next()
+// }
 
 // Bloggers
 
@@ -197,34 +189,31 @@ app.get("/bloggers/:id", (req: Request, res: Response) => {
 
 app.post("/bloggers", (req: Request, res: Response) => {
   if (!isValidYoutubeURI(req.body.youtubeURI, re)) {
-    error = {
-      data: {
-        additionalProp1: req.body.youtubeURI,
-        additionalProp2: req.body.name,
-      },
-      errorsMessage: {
-        message: "invalid youtube URI",
-        field: "youtubeURI",
-      },
-      resultCode: 1,
+    error.data = {
+      name: req.body.name,
+      youtubeURI: req.body.youtubeURI,
     };
+    error.errorsMessage = {
+      message: "invalid youtube URI",
+      field: "youtubeURI",
+    };
+    error.resultCode = 1;
     res.status(400).json(error);
     return;
   } else if (!req.body.name.trim()) {
-    error = {
-      data: {
-        additionalProp1: req.body.youtubeURI,
-        additionalProp2: req.body.name,
-      },
-      errorsMessage: {
-        message: "Blogger's name is missing, please add",
-        field: "name",
-      },
-      resultCode: 2,
+    error.data = {
+      name: req.body.name,
+      youtubeURI: req.body.youtubeURI,
     };
+    error.errorsMessage = {
+      message: "Blogger's name is missing, please add",
+      field: "name",
+    };
+    error.resultCode = 2;
     res.status(400).json(error);
     return;
   } else {
+    // add ID already exists check!!!!!!!!!!!!!!!!!!!!
     const newBlogger: BloggerType = {
       id: Number(bloggers.length + 1),
       name: req.body.name,
@@ -237,50 +226,75 @@ app.post("/bloggers", (req: Request, res: Response) => {
 
 app.put("/bloggers/:id", (req: Request, res: Response) => {
   const bloggerID = Number(req.params.id);
+  const blogger = bloggers.find((b) => b.id === bloggerID);
+  const bloggerIndex = bloggers.findIndex((b) => b.id === bloggerID);
 
-  if (isNaN(bloggerID) || bloggers.length < bloggerID) {
+  if (isNaN(bloggerID) || !blogger) {
     return res.status(404).json({
       status: "fail",
-      message: "404 not found , Invalid ID",
+      message: "Invalid ID  or blogger doesn't exists",
     });
-  } else if (req.body.name === "") {
-    return res.status(400).json({
-      status: "fail",
-      message: "Blogger's name is missing, pls add",
-    });
-  } else if (isValidYoutubeURI(req.body.youtubeURI, re)) {
-    const updatedBlogger = {
+  } else if (!isValidYoutubeURI(req.body.youtubeURI, re)) {
+    console.log("inside iURI check");
+    error.data = {
+      id: bloggerID,
+      name: req.body.name,
+      youtubeURI: req.body.youtubeURI,
+    };
+    error.errorsMessage = {
+      message: "invalid youtube URI",
+      field: "youtubeURI",
+    };
+    error.resultCode = 1;
+    res.status(400).json(error);
+    return;
+  } else if (!req.body.name.trim()) {
+    console.log("inside name check");
+    error.data = {
       id: bloggerID,
       name: req.body.name,
       youtubeURI: req.body.youtubeURI,
     };
 
-    bloggers.splice(bloggerID - 1, 1, updatedBlogger);
-    res.status(204).send("Updated");
-  } else {
-    res.status(400).json({
-      status: "fail",
-      message: "invalid youtube URI",
-    });
+    error.errorsMessage = {
+      message: "Blogger's name is missing, please add",
+      field: "name",
+    };
+    error.resultCode = 2;
+    res.status(400).json(error);
+    return;
   }
+  const updatedBlogger: BloggerType = {
+    id: bloggerID,
+    name: req.body.name,
+    youtubeURI: req.body.youtubeURI,
+  };
+
+  bloggers.splice(bloggerIndex, 1, updatedBlogger);
+  res.sendStatus(204);
 });
+
 app.delete("/bloggers/:id", (req: Request, res: Response) => {
   const bloggerID = Number(req.params.id);
-  const blogger: any = bloggers.find((b) => b.id === bloggerID);
-  if (isNaN(bloggerID) || !bloggers.find((b) => b.id === bloggerID)) {
+  const blogger = bloggers.find((b) => b.id === bloggerID);
+  const bloggerIndex = bloggers.findIndex((b) => b.id === bloggerID);
+  if (isNaN(bloggerID) || !blogger) {
     return res.status(404).json({
       status: "fail",
-      message: "404 not found , Invalid ID",
+      message: "Invalid ID  or blogger doesn't exists",
     });
   }
-  bloggers.splice(blogger["id"] - 1, 1);
-  res.status(204).send("Deleted");
+  bloggers.splice(bloggerIndex, 1);
+  res.sendStatus(204);
 });
 
 // Posts==========================================
 
 app.get("/posts", (req: Request, res: Response) => {
-  res.status(200).json(posts);
+  const postsWithBloggerNames: PostType[] = posts.map((p) =>
+    Object.assign(p, { bloggerName: bloggers.find((b) => b.id === p.bloggerID)?.name })
+  );
+  res.status(200).json(postsWithBloggerNames);
 });
 
 app.post("/posts", (req: Request, res: Response) => {
@@ -289,32 +303,63 @@ app.post("/posts", (req: Request, res: Response) => {
     req.body.shortDescription === "" ||
     req.body.content === "" ||
     req.body.bloggerID === "" ||
-    isNaN(req.body.bloggerID) ||
-    req.body.bloggerName === ""
+    isNaN(req.body.bloggerID)
   ) {
-    return res.status(400).json({
-      status: "fail",
-      message: "Wrong inputs, please check all fields",
-    });
+    error.data.title = req.body.title;
+    error.data.shortDescription = req.body.shortDescription;
+    error.data.content = req.body.content;
+    error.data.bloggerID = req.body.bloggerID;
+    error.resultCode = 3;
+
+    if (req.body.title === "") {
+      error.errorMessage = {
+        message: "Title is missing, please add",
+        field: "title",
+      };
+    } else if (req.body.shortDescription === "") {
+      error.errorMessage = {
+        message: "ShortDescription is missing, please add",
+        field: "shortDescription",
+      };
+    } else if (req.body.content === "") {
+      error.errorMessage = {
+        message: "Content is missing, please add",
+        field: "content",
+      };
+    } else if (req.body.bloggerID === "") {
+      error.errorMessage = {
+        message: "BloggerID is missing",
+        field: "bloggerID",
+      };
+      return res.status(404).send(error);
+    } else if (isNaN(req.body.bloggerID)) {
+      error.errorMessage = {
+        message: "BloggerID is invalid",
+        field: "bloggerID",
+      };
+    }
+    return res.status(400).send(error);
+  } else {
+    const newPost = {
+      id: Number(posts.length + 1),
+      title: req.body.title,
+      shortDescription: req.body.shortDescription,
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+      bloggerID: req.body.bloggerID,
+      bloggerName: req.body.bloggerName,
+    };
+
+    posts.push(newPost);
+    res.status(201).json(newPost);
   }
-
-  const newPost = {
-    id: Number(posts.length + 1),
-    title: req.body.title,
-    shortDescription: req.body.shortDescription,
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
-    bloggerID: req.body.bloggerID,
-    bloggerName: req.body.bloggerName,
-  };
-
-  posts.push(newPost);
-  res.status(201).json(newPost);
 });
 
 app.get("/posts/:id", (req: Request, res: Response) => {
   const postID: number = Number(req.params.id);
-  const post = posts.find((p) => +p.id === postID);
+
+  const post = posts
+    .map((p) => Object.assign(p, { bloggerName: bloggers.find((b) => b.id === p.bloggerID)?.name }))
+    .find((p) => +p.id === postID);
   res.status(200).json(post);
 });
 
@@ -332,13 +377,41 @@ app.put("/posts/:id", (req: Request, res: Response) => {
     req.body.shortDescription === "" ||
     req.body.content === "" ||
     req.body.bloggerID === "" ||
-    isNaN(req.body.bloggerID) ||
-    req.body.bloggerName === ""
+    isNaN(req.body.bloggerID)
   ) {
-    return res.status(400).json({
-      status: "fail",
-      message: "Invalid input. Please fill up all fields",
-    });
+    error.data.title = req.body.title;
+    error.data.shortDescription = req.body.shortDescription;
+    error.data.content = req.body.content;
+    error.data.bloggerID = req.body.bloggerID;
+    error.resultCode = 3;
+    if (req.body.title === "") {
+      error.errorMessage = {
+        message: "Title is missing, please add",
+        field: "title",
+      };
+    } else if (req.body.shortDescription === "") {
+      error.errorMessage = {
+        message: "ShortDescription is missing, please add",
+        field: "shortDescription",
+      };
+    } else if (req.body.content === "") {
+      error.errorMessage = {
+        message: "Content is missing, please add",
+        field: "content",
+      };
+    } else if (req.body.bloggerID === "") {
+      error.errorMessage = {
+        message: "BloggerID is missing, please add",
+        field: "bloggerID",
+      };
+    } else if (isNaN(req.body.bloggerID)) {
+      error.errorMessage = {
+        message: "BloggerID is invalid",
+        field: "bloggerID",
+      };
+    }
+
+    return res.status(400).send(error)
   } else if (post !== undefined) {
     const updatedPost: PostType = {
       id: postID,
@@ -346,11 +419,12 @@ app.put("/posts/:id", (req: Request, res: Response) => {
       shortDescription: req.body.shortDescription,
       content: req.body.content,
       bloggerID: req.body.bloggerID,
-      bloggerName: req.body.bloggerName,
     };
 
-    posts.splice(postID - 1, 1, updatedPost);
-    res.status(204).json({
+    const postIndex = posts.findIndex((b) => b.id === postID);
+    posts.splice(postIndex, 1, updatedPost);
+
+    res.status(200).json({
       status: "success",
       data: {
         updatedPost,
@@ -361,14 +435,16 @@ app.put("/posts/:id", (req: Request, res: Response) => {
 
 app.delete("/posts/:id", (req: Request, res: Response) => {
   const postID = Number(req.params.id);
-  if (isNaN(postID) || posts.length < postID) {
+  const post = posts.find((b) => b.id === postID);
+  const postIndex = posts.findIndex((b) => b.id === postID);
+  if (isNaN(postID) || !post) {
     return res.status(404).json({
       status: "fail",
-      message: "404 not found , Invalid ID",
+      message: "Invalid ID  or post doesn't exists",
     });
   }
-  posts.splice(postID - 1, 1);
-  res.status(204).end();
+  posts.splice(postIndex, 1);
+  res.sendStatus(204);
 });
 
 app.listen(port, () => {
