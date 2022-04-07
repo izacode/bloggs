07c71676319ef1b9b5c55626fadd1bab5f19c155
unknown <izaelec@gmail.com>
2022-04-07 09:32:36 -0400

    added repos and DB

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -1,9 +1,115 @@
-const videos = [];
-
-export const videosRepository = {
-  getVideos() {},
-  getVideoById(id: number) {},
-  deleteVideoById(id: number) {},
-  updateVideoById(id: number, title: string) {},
-  createVideo(id: number, title: string) {},
+export type PostType = {
+  id: number;
+  title: string;
+  shortDescription: string;
+  content: string;
+  bloggerID: number;
+  bloggerName?: string;
 };
+
+export type BloggerType = {
+  id: number;
+  name: string;
+  youtubeURI: string;
+};
+
+export const bloggers: BloggerType[] = [
+  {
+    id: 1,
+    name: "Blogger-01",
+    youtubeURI: "https://www.youtube.com/Blogger-01",
+  },
+  {
+    id: 2,
+    name: "Blogger-02",
+    youtubeURI: "https://www.youtube.com/Blogger-02",
+  },
+  {
+    id: 3,
+    name: "Blogger-03",
+    youtubeURI: "https://www.youtube.com/Blogger-03",
+  },
+  {
+    id: 4,
+    name: "Blogger-04",
+    youtubeURI: "https://www.youtube.com/Blogger-04",
+  },
+  {
+    id: 5,
+    name: "Blogger-05",
+    youtubeURI: "https://www.youtube.com/Blogger-05",
+  },
+];
+
+export const posts: PostType[] = [
+  {
+    id: 1,
+    title: "Post-01",
+    shortDescription: "First post of Blogger-01",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    bloggerID: 1,
+  },
+  {
+    id: 2,
+    title: "Post-01",
+    shortDescription: "First post of Blogger-01",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    bloggerID: 2,
+  },
+  {
+    id: 3,
+    title: "Post-01",
+    shortDescription: "First post of Blogger-01",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    bloggerID: 3,
+  },
+  {
+    id: 4,
+    title: "Post-01",
+    shortDescription: "First post of Blogger-01",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    bloggerID: 4,
+  },
+  {
+    id: 5,
+    title: "Post-01",
+    shortDescription: "First post of Blogger-01",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    bloggerID: 5,
+  },
+  {
+    id: 6,
+    title: "Post-02",
+    shortDescription: "Second post of Blogger-01",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    bloggerID: 1,
+  },
+  {
+    id: 7,
+    title: "Post-02",
+    shortDescription: "Second post of Blogger-02",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    bloggerID: 2,
+  },
+  {
+    id: 8,
+    title: "Post-02",
+    shortDescription: "Second post of Blogger-03",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    bloggerID: 3,
+  },
+  {
+    id: 9,
+    title: "Post-02",
+    shortDescription: "Second post of Blogger-04",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    bloggerID: 4,
+  },
+  {
+    id: 10,
+    title: "Post-02",
+    shortDescription: "Second post of Blogger-05",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, deserunt!",
+    bloggerID: 5,
+  },
+];
