b1c84c3510e9c937463ed49beb26f33a9d8e59cd
unknown <izaelec@gmail.com>
2022-04-18 18:29:30 -0400

    tests removed

--- src/repositories/bloggers-db-repository.ts
+++ src/repositories/bloggers-db-repository.ts
@@ -46,9 +46,9 @@ export const bloggersRepository = {
     return blogger !== null ? customResponse : false;
   },
   async getBlogger(id: number) {
-    // const blogger = await bloggersCollection.findOne({ id }, { projection: { _id: 0 } });
-    // return blogger;
-    return { id: 1 };
+    const blogger = await bloggersCollection.findOne({ id }, { projection: { _id: 0 } });
+    return blogger;
+    // return { id: 1 };
   },
 
   async createBlogger(newBlogger: BloggerType) {
