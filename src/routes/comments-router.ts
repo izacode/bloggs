bb983aaf123c5eeb0aabfc257bc8fa5e9db44225
izacode <izaelec@gmail.com>
2022-05-08 17:13:41 -0400

    coment content validation added

--- src/routes/comments-router.ts
+++ src/routes/comments-router.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from "express";
 import { commentsService } from "../domain/comments-service";
 import { authentication } from "../middleware/authMiddleware";
-import { contentValidation, inputValidationMiddleware, mongoIdValidation } from "../middleware/inputValidation";
+import { commentContentValidation, contentValidation, inputValidationMiddleware, mongoIdValidation } from "../middleware/inputValidation";
 import { CommentType } from "../types/types";
 
 export const commentsRouter = Router();
@@ -11,13 +11,19 @@ commentsRouter.get("/:commentId",  async (req: Request, res: Response) => {
   comment ? res.send(comment) : res.sendStatus(404);
 });
 
-commentsRouter.put("/:commentId", authentication, contentValidation, inputValidationMiddleware, async (req: Request, res: Response) => {
-  const commentToUpdate = await commentsService.getCommentById(req.params.commentId);
-  if (!commentToUpdate) return res.sendStatus(404);
-  if (commentToUpdate.userId.toString() !== req.context.user.id) return res.sendStatus(403);
-  const isUpdated = await commentsService.updateComment(req.params.commentId, req.body.content);
-  isUpdated ? res.sendStatus(204) : res.sendStatus(404);
-});
+commentsRouter.put(
+  "/:commentId",
+  authentication,
+  commentContentValidation,
+  inputValidationMiddleware,
+  async (req: Request, res: Response) => {
+    const commentToUpdate = await commentsService.getCommentById(req.params.commentId);
+    if (!commentToUpdate) return res.sendStatus(404);
+    if (commentToUpdate.userId.toString() !== req.context.user.id) return res.sendStatus(403);
+    const isUpdated = await commentsService.updateComment(req.params.commentId, req.body.content);
+    isUpdated ? res.sendStatus(204) : res.sendStatus(404);
+  }
+);
 
 commentsRouter.delete("/:commentId", authentication, async (req: Request, res: Response) => {
   const commentToDelete = await commentsService.getCommentById(req.params.commentId);
