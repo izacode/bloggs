2364a001721f4b305e38b283d45bcfba330d257d
izacode <izaelec@gmail.com>
2022-05-15 08:31:41 -0400

    final commit after ht4

--- src/routes/comments-router.ts
+++ src/routes/comments-router.ts
@@ -20,7 +20,6 @@ commentsRouter.put(
   async (req: Request, res: Response) => {
     const commentToUpdate: CommentType | null = await commentsService.getCommentById(req.params.commentId);
     if (!commentToUpdate) return res.sendStatus(404);
-    // if (commentToUpdate.userId !== req.context.user.id) return res.sendStatus(403);
     const isUpdated = await commentsService.updateComment(req.params.commentId, req.body.content);
     isUpdated ? res.sendStatus(204) : res.sendStatus(404);
   }
