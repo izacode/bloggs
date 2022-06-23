c448def8d92b726bc2b8462063094e5b3183d0e8
izacode <izaelec@gmail.com>
2022-06-23 19:37:32 -0400

    tracking requests modified

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -113,9 +113,9 @@ export const isEmailExists = async (req: Request, res: Response, next: NextFunct
   next();
 };
 export const requestCollect = async (req: Request, res: Response, next: NextFunction) => {
-  let reqNumber = 0
-  const list = await usersRepository.getAllRequests()
-  const income = { requestIp: req.ip, requestBody: req.body, url: req.url, date: new Date(), reqNumber: list.length };
+  
+ 
+  const income = { requestIp: req.ip, requestBody: req.body, url: req.url, date: new Date() };
 
   const isAdded = usersRepository.saveRequests(income);
   if (!isAdded) return res.send("Could not save request");
